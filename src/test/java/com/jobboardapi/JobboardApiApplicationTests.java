5f22bef2b974c242e61cad895f755ca73415a639
MustBeCoy <bhupps69@gmail.com>
2026-04-11 01:24:51 +0530

    GAy 1 : project setup with spring boot

---
+++ src/test/java/com/jobboardapi/JobboardApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jobboardapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JobboardApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
